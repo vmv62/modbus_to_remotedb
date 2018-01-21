9507bd813a443b5ebdf6fbae44295220d2024af7
vmv <vmv@nxt.ru>
2018-01-21 22:05:34 +0300

    daemon state

--- main.c
+++ main.c
@@ -1,16 +1,24 @@
 #include <stdio.h>
 #include <string.h>
 #include <modbus/modbus.h>
+#include <unistd.h>
 #include "defineses.h"
 
 float result_convert(uint16_t *raw_data);
 
 int main(int argc, char **argv){
 
+	pid_t pid;		//fork context
 	uint16_t buff[10];	//buffer for temporary data
 	float read_data[12]; 	//Array for store read data
 	int i=0;		//Servise variable
 	modbus_t *ctx;		//modbus context for work whith protocol
+
+	//Run programm like a daemon
+	pid = fork();
+	if(pid){
+		return -1;
+	}
 
 	ctx = modbus_new_rtu("/dev/ttyUSB0", 2400, 'N', 8, 1);
 	if (ctx == NULL) {
@@ -21,9 +29,11 @@ int main(int argc, char **argv){
 	modbus_set_slave(ctx, 1);
 	modbus_connect(ctx);
 
-	for(i; i < 13; i++){
-		read_data[i] = modbus_read_input_registers(ctx, registers[i], 0x02, buff);
-		printf("%20s: %10.2f\n", legend[i], result_convert(buff));
+	while(1){
+		for(i; i < 13; i++){
+			read_data[i] = modbus_read_input_registers(ctx, registers[i], 0x02, buff);
+			printf("%20s: %10.2f\n", legend[i], result_convert(buff));
+		}
 	}
 	modbus_free(ctx);
 	return 0;

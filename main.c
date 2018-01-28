a7a4217f1edea722502a6423d3a22cd3aa2e50e9
vmv <vmv@nxt.ru>
2018-01-28 11:21:08 +0300

    auto commit

--- main.c
+++ main.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <modbus/modbus.h>
 #include <unistd.h>
 #include "defineses.h"
-#include "mysql.h"
+#include "/home/vmv/programming/mysql/mysql_cli.h"
 
 float result_convert(uint16_t *raw_data);
 
@@ -15,11 +16,20 @@ int main(int argc, char **argv){
 	int i=0, tmp_result;		//Servise variable
 	modbus_t *ctx;		//modbus context for work whith protocol
 
+	DBDAT *db;
+	db = (DBDAT *)malloc(sizeof(DBDAT));
+
+	strcpy(db->user, "root");
+	strcpy(db->passwd, "23272829");
+	strcpy(db->server, "localhost");
+        strcpy(db->name, "sensors");
+	strcpy(db->table, "Counter");
+
 	//Run programm like a daemon
-//	pid = fork();
-//	if(pid){
-//		return -1;
-//	}
+	pid = fork();
+	if(pid){
+		return 0;
+	}
 
 	ctx = modbus_new_rtu("/dev/ttyUSB0", 2400, 'N', 8, 1);
 	if (ctx == NULL) {
@@ -30,15 +40,17 @@ int main(int argc, char **argv){
 	modbus_set_slave(ctx, 1);
 	modbus_connect(ctx);
 
-//	while(1){
+	while(1){
 		for(i; i < 13; i++){
 			modbus_read_input_registers(ctx, registers[i], 0x02, buff);
-			read_data[i] = result_convert(buff);
-//			printf("%20s: %10.2f\n", legend[i], result_convert(buff));
+			sprintf(db->col[i].data, "%.2f",  result_convert(buff));
+			strcpy(db->col[i].name, legend[i]);
+			strcpy(db->col[i].type, "DECIMAL(5,2)");
 		}
-		i=0;
-//	}
-	add_to_db(read_data);
+		db->tc_num = i;
+		i = 0;
+		add_to_db(db);
+	}
 	modbus_free(ctx);
 	return 0;
 }
